45176ca4c074fc1fff5ac8cceaf3b28f094e44f7
Shouugou <shouugou@qq.com>
2025-03-16 02:00:24 +0800

    version："1.0.0"

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+import random
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
-        super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.star import Star, register
 
-    async def terminate(self):
-        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
+# 模拟浅草寺的签文
+omikuji_results = [
+    {"result": "大吉", "description": "万事顺利，好运连连。"},
+    {"result": "吉", "description": "事情会顺利进行。"},
+    {"result": "中吉", "description": "有些小波折，但总体顺利。"},
+    {"result": "小吉", "description": "需要努力，但会有好结果。"},
+    {"result": "末吉", "description": "结果一般，需要多加小心。"},
+    {"result": "凶", "description": "可能会遇到困难，需谨慎行事。"},
+    {"result": "大凶", "description": "运势不佳，需特别小心。"},
+]
+
+@register("omikuji", "Shouugou", "浅草寺抽签插件", "1.0.0", "repo url")
+class OmikujiPlugin(Star):
+    @filter.command("抽签")
+    async def omikuji(self, event: AstrMessageEvent):
+        '''浅草寺抽签'''
+
+        # 随机选择一个签文
+        selected_omikuji = random.choice(omikuji_results)
+
+        # 返回结果
+        yield event.plain_result(f"抽签结果是：{selected_omikuji['result']}\n{selected_omikuji['description']}")
